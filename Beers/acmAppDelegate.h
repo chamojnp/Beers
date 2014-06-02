7117a26c18505abdf2f6e6d79a38b2fc180b17c6
Antonio Chamorro <achamorrom@MacBook-Pro-de-Antonio.local>
2014-06-02 17:30:17 +0200

    Initial Commit

---
+++ Beers/acmAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  acmAppDelegate.h
+//  Beers
+//
+//  Created by Antonio Chamorro on 02/06/14.
+//  Copyright (c) 2014 acm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface acmAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
